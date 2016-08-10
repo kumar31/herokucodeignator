a59e495ab56398e35e8800914b33ce94b72bacfd
Karthik Chandran <karthik.c@smaatapps.com>
2016-08-10 20:56:35 +0530

    re

--- stripe/lib/Stripe.php
+++ stripe/lib/Stripe.php
@@ -3,15 +3,7 @@
 // Tested on PHP 5.2, 5.3
 
 // This snippet (and some of the curl code) due to the Facebook SDK.
-if (!function_exists('curl_init')) {
-  throw new Exception('Stripe needs the CURL PHP extension.');
-}
-if (!function_exists('json_decode')) {
-  throw new Exception('Stripe needs the JSON PHP extension.');
-}
-if (!function_exists('mb_detect_encoding')) {
-  throw new Exception('Stripe needs the Multibyte String PHP extension.');
-}
+
 echo "hello";
 // Stripe singleton
 require(dirname(__FILE__) . '/Stripe/Stripe.php');
