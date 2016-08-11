1d90ba00acdb18d531db02b7119abb1e46f0acb8
Karthik Chandran <karthik.c@smaatapps.com>
2016-08-11 14:31:44 +0530

    rt

--- stripe/create_bank_recipient_agent.php
+++ stripe/create_bank_recipient_agent.php
@@ -1,7 +1,8 @@
 <?php
 require(dirname(__FILE__) . '/lib/Stripe.php');
 //require 'lib/Stripe.php';
-Stripe::setApiKey("sk_test_l8FVfiAVF7kbHq3oGOD2hCkL");
+$apikey = $_ENV['SECRET_KEY'];
+Stripe::setApiKey($apikey);
 
 error_reporting(0);			 
  
