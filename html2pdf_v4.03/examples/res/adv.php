a091f7051abd28055b1a0946bd8ab47587addce3
kumar <kumarappan@smaatapps.com>
2016-08-18 16:56:39 +0530

    soiree

--- html2pdf_v4.03/examples/res/adv.php
+++ html2pdf_v4.03/examples/res/adv.php
@@ -1,16 +1,5 @@
 <?php 
-
-$servername = getenv( 'DB_HOST' );
-$username = getenv( 'DB_USER' );
-$password = getenv( 'DB_PASS' );
-$dbname = getenv( 'DB_NAME' );
-
-// Create connection
-$conn = mysql_connect($servername, $username, $password);
-// Check connection
-
-mysql_select_db($dbname);
-
+require('config.php');
 $AdID=$_GET['event_id'];	
 
 //Event details
---
+++ html2pdf_v4.03/examples/res/config.php
@@ -0,0 +1,12 @@
+<?php 
+	$servername = getenv( 'DB_HOST' );
+	$username = getenv( 'DB_USER' );
+	$password = getenv( 'DB_PASS' );
+	$dbname = getenv( 'DB_NAME' );
+
+	// Create connection
+	$conn = mysql_connect($servername, $username, $password);
+	// Check connection
+
+	mysql_select_db($dbname);
+?>
