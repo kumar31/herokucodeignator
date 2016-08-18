f62efb0079e9b7ad7b130a653d1fd6b8d4b64365
kumar <kumarappan@smaatapps.com>
2016-08-18 15:05:12 +0530

    soiree

--- webservice/application/models/mail_model.php
+++ webservice/application/models/mail_model.php
@@ -8,7 +8,7 @@ class mail_model extends CI_Model {
 	
 	
 	
-	function send($to_email,$subject,$message,$from_email)
+	function send_hero($to_email,$subject,$message,$from_email)
 	{		
 	
 		$to = $to_email;
@@ -19,6 +19,38 @@ class mail_model extends CI_Model {
 		$headers .= 'Reply-To: <'.$from_email.'>' . "\r\n"; 
 		mail($to,$subject,$message,$headers);
 		
+	}
+	
+	function send($to_email,$subject,$message){
+	
+		$mail             = new PHPMailer();
+    	$body             = $message;
+    	$body             = eregi_replace("[\]",'',$body);
+    	$mail->IsSMTP(); // telling the class to use SMTP
+    	$mail->Host       = "smtp.mailgun.org"; // SMTP server
+    	$mail->SMTPDebug  = 2;           // enables SMTP debug information (for testing)
+    	                                 // 1 = errors and messages
+    	                                 // 2 = messages only
+    	$mail->SMTPAuth   = true;        // enable SMTP authentication
+    	$mail->Host       = "smtp.mailgun.org"; // sets the SMTP server
+    	$mail->Port       = 25;                    // set the SMTP port for the GMAIL server
+    	$mail->Username   = "smaatapp@auto.outfitstaff.com"; // SMTP account username
+    	$mail->Password   = ")2gNYK3Ed9K*3Z9Q4n{mWJp#Eb/rTCRq";        // SMTP account password
+    	$mail->SetFrom('support@beta.outfitstaff.com', 'outfitstaff');
+    	$mail->AddReplyTo("support@beta.outfitstaff.com","outfitstaff");
+    	$mail->Subject    = $subject;
+    	$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+    	$mail->MsgHTML($body);
+    	$address = $to_email;
+    	$mail->AddAddress($address, "outfitstaff");
+    	//$mail->AddAttachment("images/phpmailer.gif");      // attachment
+    	//$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
+    	$mail->Send();
+    	/*if(!$mail->Send()) {
+    	echo "Mailer Error: " . $mail->ErrorInfo;
+    	} else {
+    	echo "Message sent!";
+    	}*/
 	}
 	
 }
