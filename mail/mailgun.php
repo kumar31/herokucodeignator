b7360c9e35cecacbab36ad88f4b788a8472218e3
kumar31 <kumarappan@smaatapps.com>
2016-08-18 16:46:26 +0530

    Update mailgun.php

--- mail/mailgun.php
+++ mail/mailgun.php
@@ -16,7 +16,7 @@ $body = 'kumar';
 $body             = eregi_replace("[\]",'',$body);
 $mail->IsSMTP(); // telling the class to use SMTP
 $mail->Host       = "smtp.mailgun.org"; // SMTP server
-$mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
+//$mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
                                            // 1 = errors and messages
                                            // 2 = messages only
 $mail->SMTPAuth   = true;                  // enable SMTP authentication
