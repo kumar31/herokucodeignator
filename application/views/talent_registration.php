605405690927e3595b650ca7d538f925fe56ebc2
kumar <kumarappan@smaatapps.com>
2016-08-19 11:56:30 +0530

    Soiree

--- application/views/talent_registration.php
+++ application/views/talent_registration.php
@@ -617,7 +617,7 @@ include('reg_header.php'); ?>
 			}
 			
 				var url = '<?php echo $webserviceurl; ?>index.php/talent_registration';
-				
+				alert(url);
 				$.ajax({
 					'type' : 'POST',
 					'url': url,
