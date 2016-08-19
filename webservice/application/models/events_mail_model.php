d373c6c5feb9f990d76a262a53d674cd9d867362
kumar <kumarappan@smaatapps.com>
2016-08-19 12:54:58 +0530

    ds

--- webservice/application/models/events_mail_model.php
+++ webservice/application/models/events_mail_model.php
@@ -8,7 +8,7 @@ class events_mail_model extends CI_Model {
 	}
 	
 	
-	function email_hero($to_email,$to_name,$subject,$message){
+	/*function email_hero($to_email,$to_name,$subject,$message){
 	
 	   $from_email = "support@beta.outfitstaff.com";
 	  
@@ -21,11 +21,35 @@ class events_mail_model extends CI_Model {
 	   mail($to,$subject,$message,$headers);
 	   
 	 
-	}
+	}*/
 	
 	function email($to_email,$subject,$message){
 	
-		
+		$mail             = new PHPMailer();
+    	$body             = $message;
+    	$body             = eregi_replace("[\]",'',$body);
+    	$mail->IsSMTP(); // telling the class to use SMTP
+		$SMTP_HOST = getenv( 'SMTP_HOST' );
+		$SMTP_USERNAME = getenv( 'SMTP_USERNAME' );
+		$SMTP_PASSWORD = getenv( 'SMTP_PASSWORD' );
+		$SMTP_FROM = getenv( 'SMTP_FROM' );
+    	$mail->Host       = $SMTP_HOST; // SMTP server
+    	//$mail->SMTPDebug  = 2;           // enables SMTP debug information (for testing)
+    	                                 // 1 = errors and messages
+    	                                 // 2 = messages only
+    	$mail->SMTPAuth   = true;        // enable SMTP authentication
+    	$mail->Host       = $SMTP_HOST; // sets the SMTP server
+    	$mail->Port       = 25;                    // set the SMTP port for the GMAIL server
+    	$mail->Username   = $SMTP_USERNAME; // SMTP account username
+    	$mail->Password   = $SMTP_PASSWORD;        // SMTP account password
+    	$mail->SetFrom($SMTP_FROM, 'outfitstaff');
+    	$mail->AddReplyTo($SMTP_FROM,"outfitstaff");
+    	$mail->Subject    = $subject;
+    	$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+    	$mail->MsgHTML($body);
+    	$address = $to_email;
+    	$mail->AddAddress($address, "outfitstaff");
+    	$mail->Send();
     	
 	}
 	
