ba143ce7925fd3019ff0f41516e935432c6c9f06
kumar <kumarappan@smaatapps.com>
2016-08-19 12:24:49 +0530

    soiree

--- webservice/application/models/talent_registration_model.php
+++ webservice/application/models/talent_registration_model.php
@@ -52,7 +52,7 @@ class talent_registration_model extends CI_Model {
 		$talent_id = $this->db->insert_id();
 		if(($_POST['i9_form'] != '') && ($_POST['w4_form'] != '')){
 			$agent_id = $_POST['agency'];
-			$this->email($agent_id);
+			//$this->email($agent_id); 
 		}
 		
 		return $talent_id;
