15d6644247819abb3a19f0e45db6715db5067dc9
kumar31 <kumarappan@smaatapps.com>
2016-08-22 19:45:15 +0530

    a
    a

--- application/views/login.php
+++ application/views/login.php
@@ -1,5 +1,5 @@
 <?php 
-$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http";
+echo $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http";
 if($protocol == "http"){
 header("Location : https://staf.herokuapp.com/");
 }
