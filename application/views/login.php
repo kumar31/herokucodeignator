45415410d3d173ad9e82786bf192c7ac78af3f4f
kumar <kumarappan@smaatapps.com>
2016-08-23 10:29:23 +0530

    url

--- application/views/login.php
+++ application/views/login.php
@@ -1,7 +1,3 @@
-<script>
-	if (window.location.protocol != "https:")
-    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
-</script>
 <!DOCTYPE html>
 <html lang="en">
 
