7ca1d47448c9f2feaad67a107922f8d518e444de
kumar <kumarappan@smaatapps.com>
2016-08-24 13:15:12 +0530

    test

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -20,9 +20,6 @@ public function index() {
     }
     else {
       move_uploaded_file($_FILES['file']['tmp_name'], "nectorimg/".$rand.".png");
-      move_uploaded_file($_FILES['file']['tmp_name'], "/nectorimg/".$rand.".png");
-      move_uploaded_file($_FILES['file']['tmp_name'], "./nectorimg/".$rand.".png");
-      move_uploaded_file($_FILES['file']['tmp_name'], "../nectorimg/".$rand.".png");
     }
       echo getenv( 'SOIREE_BASE_URL' ) . '/nectorimg/'.$rand.'.png';
   }
