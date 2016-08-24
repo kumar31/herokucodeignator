724a4923017bc2c336463e5134b182fe8d430aa7
kumar <kumarappan@smaatapps.com>
2016-08-24 12:09:28 +0530

    test

--- application/views/talent_footer.php
+++ application/views/talent_footer.php
@@ -7,7 +7,7 @@
     </footer>
   </div>
   <!-- /container -->
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js">
   </script>
   <script src="<?php echo base_url(); ?>js/vendor/jquery-1.11.2.min.js">
   </script>
