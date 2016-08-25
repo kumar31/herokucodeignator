852fbd73b02e3ae68ab2d395a8af08f3967271d2
kumar <kumarappan@smaatapps.com>
2016-08-25 18:49:28 +0530

    chk

--- html2pdf_v4.03/examples/adv.php
+++ html2pdf_v4.03/examples/adv.php
@@ -17,7 +17,7 @@ $AdID=$_GET['event_id'];
     ob_start();
    
 	include(dirname(__FILE__).'/res/adv.php');
-    ob_get_clean();
+    $content = ob_get_clean();
 
     // convert in PDF
     require_once(dirname(__FILE__).'/../html2pdf.class.php');
