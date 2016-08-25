56fb642c9b389ea42481f7f66c32e21b8a2abdf4
kumar <kumarappan@smaatapps.com>
2016-08-25 16:13:36 +0530

    fb

--- html2pdf_v4.03/examples/adv.php
+++ html2pdf_v4.03/examples/adv.php
@@ -23,7 +23,7 @@ $AdID=$_GET['event_id'];
     ob_start();
    
 	include(dirname(__FILE__).'/res/adv.php');
-   // $content = ob_get_clean();
+    $content = ob_get_clean();
 
     // convert in PDF
     require_once(dirname(__FILE__).'/../html2pdf.class.php');
@@ -32,7 +32,7 @@ $AdID=$_GET['event_id'];
         $html2pdf = new HTML2PDF('P', 'A4', 'fr');
 //      $html2pdf->setModeDebug();
         $html2pdf->setDefaultFont('Arial'); 
-        $html2pdf->writeHTML($AdID); 
+        $html2pdf->writeHTML($content,$AdID); 
 		$my_string = substr(str_shuffle(MD5(microtime())), 0, 6); 
        // $html2pdf->Output("invoice.pdf"); 
 		//$path='../../invoice/adv'.$AdID.'.pdf';
