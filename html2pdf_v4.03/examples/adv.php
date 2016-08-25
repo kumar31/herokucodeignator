af4c2aed03fd9df574692afb9c1a84c6a3a2019c
kumar <kumarappan@smaatapps.com>
2016-08-25 11:18:42 +0530

    chk

--- html2pdf_v4.03/examples/adv.php
+++ html2pdf_v4.03/examples/adv.php
@@ -23,17 +23,17 @@ $AdID=$_GET['event_id'];
     // get the HTML http://smaatapps.com/Myshul/website_dev/index.php/invoicewithtax/index/3/
     ob_start();
    
-	include(dirname(__FILE__).'res/adv.php');
+	include(dirname(__FILE__).'/res/adv.php');
     $content = ob_get_clean();
 
     // convert in PDF
-    require_once(dirname(__FILE__).'html2pdf.class.php');
+    require_once(dirname(__FILE__).'/../html2pdf.class.php');
     try
     {
         $html2pdf = new HTML2PDF('P', 'A4', 'fr');
 //      $html2pdf->setModeDebug();
-        $html2pdf->setDefaultFont('Arial');
-        $html2pdf->writeHTML($content, isset($_GET['vuehtml'],$AdID)); echo "hi"; die;
+        $html2pdf->setDefaultFont('Arial');echo "hi"; die;
+        $html2pdf->writeHTML($content, isset($_GET['vuehtml'],$AdID)); 
 		$my_string = substr(str_shuffle(MD5(microtime())), 0, 6); 
        // $html2pdf->Output("invoice.pdf"); 
 		$path='../../invoice/adv'.$AdID.'.pdf';
