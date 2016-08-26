873fb68f66e5a3666ec5d904f59a2a6da8a07e4f
kumar <kumarappan@smaatapps.com>
2016-08-26 18:28:11 +0530

    chk

--- webservice/application/controllers/forgotpassword.php
+++ webservice/application/controllers/forgotpassword.php
@@ -55,8 +55,9 @@ class forgotpassword extends REST_Controller {
 						
 							$result = $this->forgotpassword_model->index();
 							if($result == ""){
-							$message = "Your Email ID is not registered with us. Please enter a valid Email ID.";
-							$result = $validationandresult->custommessagez($message);
+							$validation_errors = validation_errors();
+							$result = $validationandresult->formvalidation($validation_errors);
+							$this->response($result, 202);
 							}else{
 							$message = "A reset password link is sent to your registered Email ID.";
 							$result = $validationandresult->custommessage($message);
