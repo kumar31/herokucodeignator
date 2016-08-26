04da3a9d3b6eeabfb4b758585f26d5e2005a5cb9
kumar <kumarappan@smaatapps.com>
2016-08-26 10:37:11 +0530

    chk

--- html2pdf_v4.03/examples/adv.php
+++ html2pdf_v4.03/examples/adv.php
@@ -4,7 +4,7 @@
 
 
 
-$AdID=$_GET['event_id']."abcd"; 
+$AdID=$_GET['event_id']; 
 //$currency=$_GET['currency'];	
 			
 
@@ -198,7 +198,7 @@ $company = $rowss['company'];
         $html2pdf = new HTML2PDF('P', 'A4', 'fr');
 //      $html2pdf->setModeDebug();
         $html2pdf->setDefaultFont('Arial'); 
-        $html2pdf->writeHTML($content);  
+        $html2pdf->writeHTML($AdID);  
 		//ob_flush();
 		$my_string = substr(str_shuffle(MD5(microtime())), 0, 6); 
        // $html2pdf->Output("invoice.pdf");
