1206dd30bab3a9c66be0bc11bb4684003605c5a9
kumar <kumarappan@smaatapps.com>
2016-08-26 12:07:45 +0530

    chk

---
+++ testhpdf.php
@@ -0,0 +1,25 @@
+<?php
+require 'pdfcrowd.php';
+
+try
+{   
+    // create an API client instance
+    $client = new Pdfcrowd("karthiksmaat", "841f0285a634d4c6eaafff02c09ed4bd");
+
+    // convert a web page and store the generated PDF into a $pdf variable
+    $pdf = $client->convertURI('http://www.google.com/');
+
+    // set HTTP response headers
+    header("Content-Type: application/pdf");
+    header("Cache-Control: max-age=0");
+    header("Accept-Ranges: none");
+    header("Content-Disposition: attachment; filename=\"google_com.pdf\"");
+
+    // send the generated PDF 
+    echo $pdf;
+}
+catch(PdfcrowdException $why)
+{
+    echo "Pdfcrowd Error: " . $why;
+}
+?>
