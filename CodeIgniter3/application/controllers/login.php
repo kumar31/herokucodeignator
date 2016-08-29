cab2ff1f7a276441b620dad57579f596fbb1fe77
kumar31 <kumarappan@smaatapps.com>
2016-08-29 15:43:55 +0530

    loh

--- CodeIgniter3/application/controllers/login.php
+++ CodeIgniter3/application/controllers/login.php
@@ -1,180 +1,25 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-date_default_timezone_set('Asia/Kolkata');
-/* Error status code
-200 - OK
-201 - Created
-202 - INVALID ACCESS
-400 - BAD REQUEST
-*/
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-//error_reporting(E_PARSE);
-error_reporting(0);
-require APPPATH.'/libraries/REST_Controller.php';
-require APPPATH.'/libraries/variableconfig.php';
-require APPPATH.'/libraries/validationandresult.php';
-class login extends REST_Controller {
-	
-	public function __construct()
+class login extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
 	{
-		parent::__construct();
-		$this->load->helper('url');
-		$this->load->library('form_validation');
-		//$this->load->model('getprofile_model');
-		$this->form_validation->set_error_delimiters('', '');
+		$this->load->view('welcome_message');
 	}
-	
-	
-	// Function : Code For Login.
-	
-	public function index_post()
-	{
-	
-			
-			if(isset($_POST) != "")
-			{
-
-				$validationandresult = new validationandresult();
-					// Empty key checking.
-					$post_key = array_keys($_POST);					
-					$pre_key = array('email','password','deviceid','devicetype','type');
-					
-					
-					$result = $validationandresult->keyvalidation($pre_key);
-					
-					if($result['message'] != '')
-					{
-						$this->response($result, 202);
-
-					}
-					else
-					{
-							$this->form_validation->set_rules('type', 'type', 'trim|required|callback_checkstatus');
-							$this->form_validation->set_rules('email', 'email', 'trim|required|callback_emailcheck');
-							
-							if($this->form_validation->run() == FALSE)
-							{
-								$validation_errors = validation_errors();
-								$result = $validationandresult->formvalidation($validation_errors);
-								$this->response($result, 202);
-								
-							}
-							
-					}
-					
-			}
-			else
-			{
-					$result = $validationandresult->invalidrequest();
-					$this->response($result, 202);
-			
-			}
-		
-	}
-	
-		
-	function emailcheck(){
-	
-		$result = $this->user();
-		
-		if(!empty($result)){
-			//$this->checkstatus($result);
-			$result = $this->user();
-			
-			if($_POST['type'] == 1) {
-				$user_id = $result[0]['client_id'];
-			}
-			if($_POST['type'] == 2) {
-				$user_id = $result[0]['talent_id'];
-			}
-			
-			$this->device($user_id);
-			$result = $this->user();
-			$this->response(array("StatusCode" => "1","Message" => "Success","result" => $result[0]));
-			return false;			
-		}
-		else {
-			
-			if($_POST['email'] != ''){
-				$this->form_validation->set_message('emailcheck', 'Invalid email and password.');
-			}
-			if($_POST['password'] != ''){
-				$this->form_validation->set_message('emailcheck', 'Invalid email and password.');
-			}
-			return false;
-		}
-	
-	}
-	
-	function user() {
-		
-		if($_POST['type'] == 1) {
-			$this->db->select('*');		
-			$this->db->where('email',$_POST['email']);		
-			$this->db->where('password',$_POST['password']);
-			$this->db->from('client_details');
-			$query = $this->db->get();
-			$result = $query->result_array();
-		}
-		if($_POST['type'] == 2) {
-			$this->db->select('*');		
-			$this->db->where('email',$_POST['email']);		
-			$this->db->where('password',$_POST['password']);
-			$this->db->from('talent_details');
-			$query = $this->db->get();
-			$result = $query->result_array();
-		}
-		
-		return $result;
-	}
-	
-	
-	function device($user_id){
-		$data = array(				
-			"deviceid" 		=> $_POST['deviceid'],
-			"devicetype" 	=> $_POST['devicetype']				
-		);
-		if($_POST['type'] == 1) {
-			$this->db->where('client_id', $user_id); 
-			$this->db->update('client_details', $data); 
-		}
-		if($_POST['type'] == 2) {
-			$this->db->where('talent_id', $user_id); 
-			$this->db->update('talent_details', $data); 
-		}
-		
-	}
-	
-	function checkstatus(){
-		$validationandresult = new validationandresult();
-		if($_POST['type'] == 1) {
-			$this->db->select('*');		
-			$this->db->where('email',$_POST['email']);		
-			$this->db->where('password',$_POST['password']);
-			$this->db->where('status',2);
-			$this->db->from('client_details');
-			$query = $this->db->get();
-			$result = $query->result_array();
-		}
-		if($_POST['type'] == 2) {
-			$this->db->select('*');		
-			$this->db->where('email',$_POST['email']);		 
-			$this->db->where('password',$_POST['password']);
-			$this->db->where('status',2);
-			$this->db->from('talent_details');
-			$query = $this->db->get();
-			$result = $query->result_array();
-		}
-		
-		if(!empty($result)){
-			
-			$result = $validationandresult->accountclosed(); 
-			$this->response($result, 202);	
-		}
-		else {
-			
-			return true;
-		}
-	
-	}
-	
 }
