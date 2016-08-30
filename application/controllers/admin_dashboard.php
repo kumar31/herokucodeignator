388246f9ba356bd9673f30d7874ed7f10a8dd217
kumar <kumarappan@smaatapps.com>
2016-08-30 16:57:51 +0530

    chk

--- application/controllers/admin_dashboard.php
+++ application/controllers/admin_dashboard.php
@@ -8,8 +8,7 @@ public function __construct()
 			parent::__construct();
 			$this->load->helper('url');
 			$this->load->helper('form');
-			$this->load->helper('cookie');
-			$this->load->library('form_validation');
+			$this->load->helper('cookie');	
 			$this->load->library('encrypt');
 	
 	}
