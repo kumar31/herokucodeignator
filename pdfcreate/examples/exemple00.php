6fb00206fc6cffb9003aa4087058d10752afacdb
kumar <kumarappan@smaatapps.com>
2016-09-06 18:48:04 +0530

    chk

--- pdfcreate/examples/exemple00.php
+++ pdfcreate/examples/exemple00.php
@@ -19,7 +19,7 @@
     $content = ob_get_clean();
 
     // convert in PDF
-    require_once('/../vendor/autoload.php');
+    require_once('../vendor/autoload.php');
     try
     {
         $html2pdf = new HTML2PDF('P', 'A4', 'fr');
