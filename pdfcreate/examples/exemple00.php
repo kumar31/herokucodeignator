95af488fa02ff55fbd502926bc9a9f9f5eb1a0a4
kumar <kumarappan@smaatapps.com>
2016-09-07 12:17:59 +0530

    chk

--- pdfcreate/examples/exemple00.php
+++ pdfcreate/examples/exemple00.php
@@ -19,7 +19,7 @@
     $content = ob_get_clean();
 
     // convert in PDF
-    include('/../autoload.php'); echo "hi"; die;
+    include('/../autoload.php'); 
     try
     {
         $html2pdf = new HTML2PDF('P', 'A4', 'fr');
