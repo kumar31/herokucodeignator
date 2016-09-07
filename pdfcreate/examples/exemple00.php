d0dc4170b78dfc1553f515d2327cca5a2fade378
kumar <kumarappan@smaatapps.com>
2016-09-07 12:50:10 +0530

    chk

--- pdfcreate/examples/exemple00.php
+++ pdfcreate/examples/exemple00.php
@@ -22,7 +22,7 @@
     require('/../autoload.php'); 
     try
     { 	
-		ob_get_flush();
+		ob_get_clean();
         $html2pdf = new HTML2PDF('P', 'A4', 'fr'); echo "hi"; die;
 //      $html2pdf->setModeDebug();
         $html2pdf->setDefaultFont('Arial'); 
