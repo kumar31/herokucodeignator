7da98f01a8f322bda7041900bd411b84a6a9b4ff
kumar <kumarappan@smaatapps.com>
2016-09-07 12:25:32 +0530

    chk

--- pdfcreate/examples/exemple00.php
+++ pdfcreate/examples/exemple00.php
@@ -22,7 +22,7 @@
     include('/../autoload.php'); 
     try
     { 
-        $html2pdf = new HTML2PDF('P', 'A4', 'fr');echo "hi"; die;
+        $html2pdf = new HTML2PDF('P', 'A4', 'fr'); echo "hi"; die;
 //      $html2pdf->setModeDebug();
         $html2pdf->setDefaultFont('Arial'); 
         $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
